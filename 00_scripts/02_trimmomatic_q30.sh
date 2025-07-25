52f3553339b09faeeef719bd7154a83dc3c79089
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-25 11:47:28 +0200

    Update 02_trimmomatic_q30.sh

--- 00_scripts/02_trimmomatic_q30.sh
+++ 00_scripts/02_trimmomatic_q30.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=09_HOPS_reduced_01
+#SBATCH --job-name=02_trimmomatic_q30
 #SBATCH --ntasks=1
 #SBATCH -p smp
 #SBATCH --mem=1000G
 #SBATCH --mail-user=pierrelouis.stenger@gmail.com
 #SBATCH --mail-type=ALL 
-#SBATCH --error="/home/plstenge/PaleoENV/00_scripts/09_HOPS_reduced_01.err"
-#SBATCH --output="/home/plstenge/PaleoENV/00_scripts/09_HOPS_reduced_01.out"
+#SBATCH --error="/home/plstenge/PaleoENV/00_scripts/02_trimmomatic_q30.err"
+#SBATCH --output="/home/plstenge/PaleoENV/00_scripts/02_trimmomatic_q30.out"
 
 # trimmomatic version 0.39
 # trimmomatic manual : http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/TrimmomaticManual_V0.32.pdf
 
-WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/Fastq
-OUTPUT=/home/plstenge/PaleoENV/03_cleaned_data
+############################################################################################################################################
+# trnL
+############################################################################################################################################
+
+WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/Trnl/trnLgh
+OUTPUT=/home/plstenge/PaleoENV/03_cleaned_data/trnL
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
@@ -22,7 +26,8 @@ ADAPTERFILE=/home/plstenge/PaleoENV/99_softwares/adapters_sequences.fasta
 # Arguments :
 # ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:26:30 MINLEN:150
 
-eval "$(conda shell.bash hook)"
+module load conda/4.12.0
+source ~/.bashrc
 conda activate trimmomatic
 
 cd $WORKING_DIRECTORY
@@ -31,13 +36,47 @@ cd $WORKING_DIRECTORY
 # Cleaning step
 ####################################################
 
+for R1 in *R1*
+do
+   R2=${R1//R1.fastq.gz/R2.fastq.gz}
+   R1paired=${R1//.fastq.gz/_paired.fastq.gz}
+   R1unpaired=${R1//.fastq.gz/_unpaired.fastq.gz}	
+   R2paired=${R2//.fastq.gz/_paired.fastq.gz}
+   R2unpaired=${R2//.fastq.gz/_unpaired.fastq.gz}	
+
+   trimmomatic PE -Xmx60G -threads 8 -phred33 $R1 $R2 $OUTPUT/$R1paired $OUTPUT/$R1unpaired $OUTPUT/$R2paired $OUTPUT/$R2unpaired ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:26:30 MINLEN:150
+
+done ;
+
+
+############################################################################################################################################
+# ITS2
+############################################################################################################################################
+
+WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/ITS2/ITS2
+OUTPUT=/home/plstenge/PaleoENV/03_cleaned_data/ITS2
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+ADAPTERFILE=/home/plstenge/PaleoENV/99_softwares/adapters_sequences.fasta
+
+# Arguments :
+# ILLUMINACLIP:"$ADAPTERFILE":2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:26:30 MINLEN:150
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate trimmomatic
+
+cd $WORKING_DIRECTORY
+
 ####################################################
 # Cleaning step
 ####################################################
 
 for R1 in *R1*
 do
-   R2=${R1//R1_001.fastq.gz/R2_001.fastq.gz}
+   R2=${R1//R1.fastq.gz/R2.fastq.gz}
    R1paired=${R1//.fastq.gz/_paired.fastq.gz}
    R1unpaired=${R1//.fastq.gz/_unpaired.fastq.gz}	
    R2paired=${R2//.fastq.gz/_paired.fastq.gz}
