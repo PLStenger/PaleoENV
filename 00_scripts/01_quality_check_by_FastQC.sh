c92460cd1a2bbc3cab4cba86379b1620abf66222
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-07-25 11:42:06 +0200

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -13,14 +13,18 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
+############################################################################################################################################
+# trnL
+############################################################################################################################################
 
-WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/Fastq
-OUTPUT=/home/plstenge/PaleoENV/02_quality_check
+WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/Trnl/trnLgh
+OUTPUT=/home/plstenge/PaleoENV/02_quality_check/trnL
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
 
-eval "$(conda shell.bash hook)"
+module load conda/4.12.0
+source ~/.bashrc
 conda activate fastqc
 
 cd $WORKING_DIRECTORY
@@ -38,3 +42,31 @@ conda activate multiqc
 
 multiqc $OUTPUT
 
+############################################################################################################################################
+# ITS2
+############################################################################################################################################
+
+WORKING_DIRECTORY=/home/plstenge/PaleoENV/01_raw_data/ITS2/ITS2
+OUTPUT=/home/plstenge/PaleoENV/02_quality_check/ITS2
+
+# Make the directory (mkdir) only if not existe already(-p)
+mkdir -p $OUTPUT
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate fastqc
+
+cd $WORKING_DIRECTORY
+
+for FILE in $(ls $WORKING_DIRECTORY/*.fastq.gz)
+do
+      fastqc $FILE -o $OUTPUT
+done ;
+
+
+conda deactivate fastqc
+conda activate multiqc
+
+# Run multiqc for quality summary
+
+multiqc $OUTPUT
